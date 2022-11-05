1ec88571d46589d3a518e5617c3167e53597db3b
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-05 20:21:11 -0300

    bora front >>

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +18,17 @@ import com.example.demo.service.CourseService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 
+@CrossOrigin(origins = "http:/localhost:8080")
 @RequestMapping("/courses")
 @RestController
 @AllArgsConstructor
+
 public class CourseController {
 	
 	private CourseService courseService;
 	
 	@ApiOperation(value = "fetch all courses")
+
 	@GetMapping
 	public ResponseEntity<List<Course>> getAllCourses() {
 		
